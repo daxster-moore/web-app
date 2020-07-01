d960191a307510a9da818f3760e406ae8934653e
daxster-moore <daxster.moore@gmail.com>
2020-07-01 08:47:46 -0600

    password-generator init commit

---
+++ src/app/password-generator/password-generator.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-password-generator',
+  templateUrl: './password-generator.component.html',
+  styleUrls: ['./password-generator.component.scss'],
+})
+export class PasswordGeneratorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
---
+++ src/app/password-generator/password-generator.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+
+
+import { PasswordGeneratorComponent } from './password-generator.component';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        RouterModule.forChild([
+            {
+                path: '',
+                component: PasswordGeneratorComponent
+            }
+        ])
+    ],
+    declarations: [PasswordGeneratorComponent],
+
+})
+export class PasswordGeneratorModule { }
